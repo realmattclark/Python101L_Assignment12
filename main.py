feb4b0a0ee03be4a355ffb13f230a2c698a14f82
realmattclark <matt.clark5991@gmail.com>
2021-12-01 14:53:34 -0600

    initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import turtle
+
+class Point(object):
+
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+    
+    def draw(self):
+        turtle.penup()
+        turtle.goto(self.x, self.y)
+        turtle.pendown()
+        turtle.color(self.color)
+        turtle.setheading(0)
+        self.draw_action()
+    
+    def draw_action(self):
+        turtle.dot()
+p = Point(-100, 100, "blue")
+p.draw()
